5bcbe5d3af4e0b079e44ec8f8f05b9bcfb22dce5
evelynmotatech <evelynmota.arq@gmail.com>
2021-12-03 16:20:52 -0300

    Repeticoes

--- cap4_Repeticoes/exercicio_01/script.js
+++ cap4_Repeticoes/exercicio_01/script.js
@@ -1,1 +1,34 @@
 //Programa que repete o nome da fruta de acordo com o número informado.
+
+function repetirFruta() {
+
+  var inFruta = document.getElementById("inFruta");
+  var inNumero = document.getElementById("inNumero");
+  var outRepeticao = document.getElementById("outRepeticao");
+
+  fruta = inFruta.value;
+  num = Number(inNumero.value);
+  outRepeticao.innerHTML = "";
+  var i = 0;
+
+  if ((fruta == "") || (num == "") || (isNaN(num)) || (!isNaN(fruta)) || (num < 1)) {
+    outRepeticao.innerHTML = "Digite um número maior que 0 e o nome de uma fruta";
+    inFruta.value = "";
+    inNumero.value = "";
+    inFruta.focus();
+    return;
+  }
+
+  while (i < num) {
+    i++;
+    if(i == num){
+      outRepeticao.innerHTML += fruta;
+    }else{
+    outRepeticao.innerHTML += `${fruta} * `;
+    }    
+  }
+
+}
+
+var btVerificar = document.getElementById('btVerificar');
+btVerificar.addEventListener('click', repetirFruta);
