f16b2d71cf51b373ef1699085f68f994b009fa43
evelynmotatech <evelynmota.arq@gmail.com>
2021-12-06 11:10:47 -0300

    Repeticoes

--- cap4_Repeticoes/exercicio_04/script.js
+++ cap4_Repeticoes/exercicio_04/script.js
@@ -1,1 +1,20 @@
-//Programa para ler o número de linhas inseridas pelo usuário e imprimir escrelas de asteriscos.
+//Programa para ler o número de linhas inseridas pelo usuário e imprimir asteriscos.
+
+function fabricaEstrelas(numeroLinhas) {
+
+  var inNumeroLinhas = document.getElementById("inNumeroLinhas").value;
+  var outEstrelas = document.getElementById("outEstrelas");
+
+  var numeroLinhas = Number(inNumeroLinhas);
+
+  for (var i = 1; i <= numeroLinhas; i++) {
+    var estrelas = "";
+    for (var j = 1; j <= i; j++) {
+      estrelas += "*";
+    }
+    outEstrelas.innerHTML += estrelas + "<br>";
+  }
+  
+}
+var btVerificar = document.getElementById('btVerificar');
+btVerificar.addEventListener('click', fabricaEstrelas);
